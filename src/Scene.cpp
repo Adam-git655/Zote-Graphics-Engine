0b69adb77fb3940f29d1358ad266bd045ff496dc
Adamya <adamyagupta2009@gmail.com>
2026-06-05 01:09:58 +0530

    Added in both backpack and water monke in the same scene.

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -10,6 +10,9 @@ void Scene::Setup(Shader& mainShader, Shader& lightCubeShader)
 
 	models.emplace_back(RESOURCES_PATH"objects/backpack/backpack.obj");
 	objects.push_back(std::make_unique<GameObject>("backpack", &mainShader, &models.back()));
+
+	models.emplace_back(RESOURCES_PATH"objects/monke/waterMonke.obj");
+	objects.push_back(std::make_unique<GameObject>("waterMonke", &mainShader, &models.back()));
 }
 
 void Scene::Draw(Camera& camera, glm::mat4 projection)
@@ -24,6 +27,15 @@ void Scene::Draw(Camera& camera, glm::mat4 projection)
 		//set view matrix
 		glm::mat4 view = camera.GetViewMatrix();
 		obj->shader->setMat4("view", view);
+
+		if (obj->name == "backpack")
+		{
+			obj->transform.position.x = -2;
+		}
+		else if (obj->name == "waterMonke")
+		{
+			obj->transform.position.x = 2;
+		}
 
 		obj->Draw();
 	}
